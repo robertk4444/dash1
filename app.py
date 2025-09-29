2404eb769ada58b67185e5d7e963e1f6b094d2ee
robertk4444 <robertk4444@gmail.com>
2025-09-29 22:18:11 +0200

    app.py

--- app.py
+++ app.py
@@ -74,5 +74,8 @@ def update_charts(payment_methods):
 
     return fig1, fig2, fig3
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=False)
